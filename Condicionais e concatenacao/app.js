dbd32251c5b54c685750b9ff6c3e57ac042e226f
YuriOliver085 <armandoyuri2013@yahoo.com.br>
2024-12-10 00:18:42 -0300

    Condicionais e concatenacao

--- Condicionais e concatenacao/app.js
+++ Condicionais e concatenacao/app.js
@@ -1,30 +1,31 @@
-// Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
-alert('Boas vindas ao nosso site!')
-
-// Declare uma variável chamada nome e atribua a ela o valor "Lua".
-let Nome = 'Lua';
-
-// Crie uma variável chamada idade e atribua a ela o valor 25.
-let idade = 25;
-
-// Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-let numeroDeVendas = 50;
-
-// Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-let saldoDisponivel = 1000;
-
-// Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
-let mensagemDeErro = 'Erro! Preencha todos os campos';
-alert(mensagemDeErro);
-
-// Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
-nome = prompt('Qual o seu nome?');
-
-// Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
-idade = prompt('digite sua idade');
-
-//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade. Agora, a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-idade = prompt('Digite a sua idade');
-if (idade >= 18) {
-    alert('Pode tirar a habilitação!'); 
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+let = prompt("Qual dia da semana?");
+if (dia == "Sábado" || dia == "Domingo") {
+  alert("Bom fim de semana!");
+} else {
+  alert("Boa semana!");
 }
+
+// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+numero = prompt("Digite um número:");
+if (numero > 0) {
+  alert("O número é Positivo");
+} else if (numero < 0) {
+  alert("O número é Negativo");
+}
+
+// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+if (pontos >= 100) {
+   alert("Parabéns, você venceu!");
+} else {
+   alert("Tente novamente para ganhar.");
+  }
+
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+var saldo = 2000;
+  alert(`Seu saldo é de R$ ${saldo}`)
+
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+nome = prompt("Insira seu nome:");
+alert(`Bem-vindo, ${nome}`);
+  
